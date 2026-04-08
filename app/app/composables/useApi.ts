7eba2720981e2cd9a998705b4d6af0d140cc4bd0
Carlo Dev Unstated <dev@unstated.co>
2026-04-08 15:26:50 +0200

    up

--- app/app/composables/useApi.ts
+++ app/app/composables/useApi.ts
@@ -2,12 +2,12 @@ export const useApi = () => {
   const config = useRuntimeConfig()
   
   const fetchDocs = async (collection: string) => {
-    return await $fetch(`${config.public.payloadUrl}/api/${collection}`)
-    /* const url = `${config.public.payloadBaseUrl}/api/${collection}`
+    //return await $fetch(`${config.public.payloadUrl}/api/${collection}`)
+    const url = `${config.public.payloadBaseUrl}/api/${collection}`
     console.log('[useApi] fetching:', url)
     const result = await $fetch(url)
     console.log('[useApi] response:', result)
-    return result */
+    return result
   }
 
   return { fetchDocs }
